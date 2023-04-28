f7e119d4165465d00e5bb3c4368ce71e49a55cce
nandiinii <ge87kif@mytum.de>
2023-04-28 21:55:10 +0200

    adding file

---
+++ retriving_post.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+# *title string, content string, category, etc..*
+
+class Post(BaseModel):
+    title: str 
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+#create post
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, 
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+#method to find a post
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+#updating posts
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+
+    # *declaring random number to id*
+    post_dict['id'] = randrange(0, 100000)
+
+    # *convert pydantic model into dictionary*
+    my_posts.append(post_dict)
+
+    # *send back the created post the array into dictionary*
+    return {"data": post_dict}
+
+#retriving individual post and finding that post
+@app.get("/posts/{id}")
+def get_post(id: int):          # *(id: int) automatically converts into integer*
+    post = find_post(int(id))   # *whenever we have path parameter, always return as int.*
+    print(post)
+    return {"post_detail": post}
